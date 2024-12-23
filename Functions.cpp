419ba493580d512ca00514f2303ce5ff3f492591
Ieva Prevelyte <56304202+ievaprev@users.noreply.github.com>
2024-12-23 23:45:15 +0100

    Update Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -5,7 +5,7 @@
 string& textCleaning(string& line) {
 
    line.erase(remove_if(line.begin(), line.end(), [](char character) {
-        return ::ispunct(character) || isdigit(character) || character == '-' || character == '–' || character == '—';
+        return ::ispunct(character) || isdigit(character) || character == '-' || character == 'â€”' || character == 'â€”';
         }), line.end());
 
    transform(line.begin(), line.end(), line.begin(), [](unsigned char character) {
@@ -32,19 +32,61 @@ void wordCount(string& line, map<string, Info>& words, int& count) {
     }
 }
 
+//This function is used to put all possible domains into a single set
+set<string> URLCode(string& fileName) {
+
+    ifstream inFile(fileName);
+    set<string> urlCode;
+
+    try {
+        if (!inFile.is_open())
+        {
+            throw runtime_error("Error while openning the file: " + fileName);
+        }
+        
+        string line;
+
+        while (getline(inFile, line)) {
+
+            transform(line.begin(), line.end(), line.begin(), [](unsigned char character) {
+                return tolower(character);
+                });
+
+            if (!line.empty()) {
+                urlCode.insert(string(".") + line); 
+                
+            }
+        }
+        inFile.close();
+    }
+    catch (const exception& e) {
+        cout << e.what() << endl;
+        exit(EXIT_FAILURE);
+    }
+    return urlCode; 
+}
+
 //This function uses regex to find every possible URL
-void findURL(string&line) {
+void findURL(string& line, const set<string> urlCode) {
 
-    regex regexCode(R"((?:https:\/\/|http:\/\/|www\.)[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,3}(?:\?[a-zA-Z0-9\-._~:/?#\[\]@!$&'()*+,;=%]*)?)");
+    istringstream iss(line);
+    string word;
+    set<string> urls;
 
-    string line1 = line;
-    smatch urls;
+    while (iss >> word) {
 
-    cout << "URL links: " << endl;
-
-    while (regex_search(line, urls, regexCode)){
-       
-        cout << urls.str() << endl;
-        line1 = urls.suffix().str();
+        while (!word.empty() && ispunct(word.back())) {
+            word.pop_back();
+        }
+        
+        for (const auto& url : urlCode) {
+            if (word.find(url) != string::npos)
+            {
+                urls.insert(word);
+            }
+        }
+    }
+    for (const auto& url : urls) {
+        cout << url << endl;
     }
 }
