10d75ff5d97b502b910a695c6e515911df982eb0
Ieva Prevelyte <56304202+ievaprev@users.noreply.github.com>
2024-12-23 23:47:10 +0100

    Update Methods.h

--- Methods.h
+++ Methods.h
@@ -6,9 +6,9 @@ public:
 	set<int> lines;
 };
 
-void findURL(string& line);
+void findURL(string& textLine, const set<string> urlCode);
+set<string> URLCode(string& fileName);
 string& textCleaning(string& line); 
 void wordCount(string& line, map<string, Info>& words, int& count);
 void readFile(string& fileName, map<string, Info>& words);
 void outputFile(const string& fileName, map<string, Info>& words);
- 
